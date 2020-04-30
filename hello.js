ce96013527d29d8e959c92f2c51a216958622674
Nialys <sylvain.petit@esigetel.net>
2020-04-30 12:28:17 +0200

    Commit depuis la seconde version - other branch

--- hello.js
+++ hello.js
@@ -2,7 +2,7 @@
 let message = "Revenu sur Master, après creation de branche initiative2";
 
 /// Creation de la variable
-let hello2="Hello World";
+let hello2="Bienvenue sur la seconde branche de ce repo";
 
 ///alerting avec le message
 alert(hello2);
